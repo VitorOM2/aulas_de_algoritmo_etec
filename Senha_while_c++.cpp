218c4a5bc3db6756ebec5b7a1a77376d91a546c5
Vitor de Oliveira Marques <58312845+VitorOM2@users.noreply.github.com>
2020-02-05 12:30:09 -0300

    Adicionado comentários
    Foi adicionados comentários explicando o algoritmo e  as funções dos blocos no programa

--- Senha_while_c++.cpp
+++ Senha_while_c++.cpp
@@ -1,18 +1,29 @@
+/*Declaração das bibliotecas*/
 #include<stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-
+/*Fim da declaração das bibliotecas*/
+/*Algoritmo que um login com senha.Se errar a senha três vezes o programa fecha*/
 int main (){
+	/*Deixa o algoritmo em português*/
 	setlocale (LC_ALL,"Portuguese");
+	
+	/*Declaração das variaveis*/ 
 	int resp;
 	int i=0;
-	
+	/*Declaração da senha*/
 	char senha[6]="asdfg";
-
-	while (i<3){	
+	/*Fim da declaração das variaveis*/
+	
+	/*Laço de repetição que conta os erros*/
+	while (i<3){
+	
+	/*pergunta para digitar a senha*/
 	printf ("Digite a senha: ");
 	scanf ("%s",senha);
+		
+		/*Laço de decisão*/ 
 		if (strcmp(senha,"asdfg")==0){
 			system ("CLS");
 			printf ("Bem vindo(a)!\n\n");
