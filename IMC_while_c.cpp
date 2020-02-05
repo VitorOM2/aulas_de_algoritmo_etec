bfcc6af94b56aa056ef749421891ce2646b88a3a
Vitor de Oliveira Marques <58312845+VitorOM2@users.noreply.github.com>
2020-02-05 11:30:08 -0300

    Adicionado comentários
    Foi adicionados comentários explicando o algoritmo e  as funções dos blocos no programa

--- IMC_while_c.cpp
+++ IMC_while_c.cpp
@@ -1,58 +1,102 @@
+/*Declaração das bibliotecas*/
 #include<stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <math.h>
+/*Fim da declaração das bibliotecas*/
 
+/*Algoritmo que calcula o IMC*/
 int main (){
+	
+	/*Função que deixa o algoritmo reconhecer caracteres em português*/
 	setlocale (LC_ALL,"Portuguese");
+	/**/
+	
+	/*Declaração das variaveis*/
 	int res;
 	float imc,peso,altura;
+	/*Fim da declaração das variaveis*/
 	
+	/*Pergunta inicial*/
 	printf ("Deseja calcular o seu IMC?\n");
 	printf ("1 - SIM ou 2 - NÃO: ");
 	scanf ("%d",&res);
+	
+	/*Comando para limpar a tela*/
 	system ("CLS");
 	
+	/*Laço de repetição que depende da pergunta inicial*/
 	while (res==1){
+		
+		/*Perguntas para pegar os dados da conta*/
 		printf ("Digite sua altura: ");
 		scanf ("%f",&altura);
 		
 		printf ("Digite o quanto você está pesando: ");
 		scanf ("%f",&peso);
 		
+		/*Calculo do IMC*/
 		imc = peso /(altura*altura);
 		
+		/*Decisão encadeada heterogênea*/
+		
+		/*Resultado*/
 		if ( imc < 18){
 			printf ("Sua classificação é: Magreza\n");
+			
+			/*pergunta para reiniciar o loop*/
 			printf ("Deseja calcular o seu IMC?\n");
 			printf ("1 - SIM ou 2 - NÃO: ");
 			scanf ("%d",&res);
+			
+			/*comando para limpar a tela*/
 			system ("CLS");			
 		}
+		
 		else {	
+			/*Resultado*/
 			if(imc > 18 && imc<24 ){
 			printf ("Sua classificação é: Saudável\n");
-			printf ("Deseja calcular o seu IMC?\n");
+				
+			/*pergunta para reiniciar o loop*/	
+			printf ("Deseja calcular novamente o seu IMC?\n");
 			printf ("1 - SIM ou 2 - NÃO: ");
 			scanf ("%d",&res);
+				
+			/*comando para limpar a tela*/
 			system ("CLS");
 			}
-			else{			
+			
+			else{	
+				/*Resultado*/
 				if (imc > 25,0 && imc<= 29,9){
 				printf ("Sua classificação é: Sobrepeso\n");
-				printf ("Deseja calcular o seu IMC?\n");
+					
+				/*pergunta para reiniciar o loop*/	
+				printf ("Deseja calcular novamente o seu IMC?\n");
 				printf ("1 - SIM ou 2 - NÃO: ");
 				scanf ("%d",&res);
+					
+				/*comando para limpar a tela*/					
 				system ("CLS");}
+				
 				else{
+					/*Resultado*/
 					 if (imc <= 30,0)
 					printf ("Sua classificação é; Obesidade\n");
-					printf ("Deseja calcular o seu IMC?\n");
+					
+					/*pergunta para reiniciar o loop*/
+					printf ("Deseja calcular novamente o seu IMC?\n");
 					printf ("1 - SIM ou 2 - NÃO: ");
 					scanf ("%d",&res);
+					
+					/*comando para limpar a tela*/
 					system ("CLS");			
 				}
 			}
 		}
 	}
+	/*Fim da decisão heterogênea*/
+	return 0;
 }
+
