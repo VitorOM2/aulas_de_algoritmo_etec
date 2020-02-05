1bf6b7a7443452c3eb7913e65f568eb4131e29da
Vitor de Oliveira Marques <58312845+VitorOM2@users.noreply.github.com>
2020-02-05 12:02:51 -0300

    Adicionado comentários
    Foi adicionados comentários explicando o algoritmo e  as funções dos blocos no programa

--- Peso_while_c++.cpp
+++ Peso_while_c++.cpp
@@ -1,33 +1,49 @@
+/*Declaração das bibliotecas*/
 #include<stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <windows.h>
+/*Fim da declaração das bibliotecas*/
 
+/*Algoritmo que informa o peso ideal de 15 pessoas*/
 int main()
 {
-setlocale (LC_ALL,"Portuguese");
-float altura, pesomasc, pesofem, sexo;
-int i=0;
-while (i<15){
-
-printf ("Informe sua altura: "); 
-scanf("%f",&altura);
-printf ("Informe seu sexo 0-para Msculino 1-para Feminino: "); 
-scanf	("%f",&sexo);
-
-i++;
+   /*Função que deixa o algoritmo reconhecer caracteres em português*/
+	   setlocale (LC_ALL,"Portuguese");
+   /**/
  
-if (sexo == 0){
-pesomasc = (72.7 * altura) - 58;
-printf("Peso ideal = %.2f\n\n",pesomasc);
-}
+    /*Declaração das variaveis*/ 
+	   float altura, pesomasc, pesofem, sexo;
+	   int i=0;
+    /*Fim da declaração das variaveis*/
  
-else if (sexo== 1) {
-pesofem = (62.1 * altura) - 44.7;
-printf("Peso ideal = %.2f\n\n",pesofem);
-}
-}
+	   /*Laço de repetição para contar quantas pessoas ja fizeram o procedimento*/
+	   while (i<15){
+       
+        /*perguntas para coletar os dados*/
+		      printf ("Informe sua altura: "); 
+		      scanf("%f",&altura);
+     
+		      printf ("Informe seu sexo 0-para Msculino 1-para Feminino: "); 
+		      scanf	("%f",&sexo);
+
+        /*Aumenta em 1 o contador de pessoas*/
+		      i++;
+    /*Estrutura de decisão*/
+		  if (sexo == 0){
+       /*conta para calcular o peso ideal masculino*/
+			    pesomasc = (72.7 * altura) - 58;
+       /*resultado*/
+			    printf("Peso ideal = %.2f\n\n",pesomasc);
+		}
+ 
+		  else if (sexo== 1) {
+       /*conta para calcular o peso ideal feminino*/
+		     pesofem = (62.1 * altura) - 44.7;
+       /*resultado*/
+		     printf("Peso ideal = %.2f\n\n",pesofem);
+		}
+	}
  
 return 0;
 }	
- 
